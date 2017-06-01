9b910a86f0b31294debe3bc06b652b02c7b33894
意绮 <fangjun.yfj@alibaba-inc.com>
2017-06-01 16:52:09 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/**
+ * @file        滚动插件
+ * @author      fangjun.yfj
+ */
+
+/**
+ * Tiny.js
+ * @external Tiny
+ * @see {@link http://tinyjs.net/}
+ */
+
+/**
+ * 滚动插件
+ *
+ * @class Scroller
+ */
+import Scroller from './Scroller';
+
+module.exports = Scroller;
